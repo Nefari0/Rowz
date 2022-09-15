977b8e94399620889eb73ae2fc538444485a2b6f
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-15 14:54:36 -0600

    Adjustments

--- CheckerBoard/board.styles.jsx
+++ CheckerBoard/board.styles.jsx
@@ -4,7 +4,7 @@ export const CheckerTable = styled.main`
     position: absolute;
     height: 350px;
     width: 350px;
-    margin-top: 30px;
+    margin-top: 70px;
     background-color: rgb(240, 240, 240);
     background-image: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0)),url(white-backround1.jpg);
     display: flex;
@@ -13,7 +13,7 @@ export const CheckerTable = styled.main`
     justify-content: center;
     align-items: flex-start;
     align-content: stretch;
-    overflow: hidden;
+
     box-shadow: 0px 5px 20px -7px #000000;
     z-index: 0;
 `
