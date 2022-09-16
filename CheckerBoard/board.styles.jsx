ab5faacaa856bb7f769d3e109f51a2635c4ae1e7
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-16 05:14:54 -0600

    Screen size

--- CheckerBoard/board.styles.jsx
+++ CheckerBoard/board.styles.jsx
@@ -13,9 +13,15 @@ export const CheckerTable = styled.main`
     justify-content: center;
     align-items: flex-start;
     align-content: stretch;
-
     box-shadow: 0px 5px 20px -7px #000000;
     z-index: 0;
+
+    @media (max-width:400px) {
+        -webkit-transform: scale(.8);
+        -ms-transform: scale(.8);
+        transform: scale(.8);
+        margin-left:-30px;
+    }
 `
 
 export const Rowz = styled.span`
